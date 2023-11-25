bf65de2887db73993fbd035c9e0fd5c2b615ff79
Major2000 <majormadobe@gmail.com>
2023-11-25 18:49:00 +0300

    update urls.py and urls.py

--- restraurant_activities_management/urls.py
+++ restraurant_activities_management/urls.py
@@ -4,9 +4,9 @@ from .views import *
 app_name = 'restraurant_activities_management'
 
 urlpatterns = [
-    path('/api/restaurant', RestaurantView.as_view()),
-    path('/api/coupon', CouponView.as_view()),
-    path('/api/awards', AwardView.as_view()),
-    path('/api/owner-restaurant', OwnerRestaurantView.as_view()),
-    path('/api/use_points', CouponTransactionView.as_view()),
+    path('api/restaurant', RestaurantView.as_view()),
+    path('api/coupon', CouponView.as_view()),
+    path('api/awards', AwardView.as_view()),
+    path('api/owner-restaurant', OwnerRestaurantView.as_view()),
+    path('api/use_points', CouponTransactionView.as_view()),
 ]
