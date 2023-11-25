f379aa0d71d90084abc939bf40d57df81d7e1a02
Major2000 <majormadobe@gmail.com>
2023-11-25 18:32:06 +0300

    update serializers.py

--- business_management/serializers.py
+++ business_management/serializers.py
@@ -1,7 +1,4 @@
-from django.contrib.auth import authenticate
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from django.contrib.auth.forms import PasswordChangeForm
 from .models import User
 
 
