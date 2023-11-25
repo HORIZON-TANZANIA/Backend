c6a231c5ceadcd0a7a16ecbef115f9edaf2cde3b
Major2000 <majormadobe@gmail.com>
2023-11-25 18:42:00 +0300

    feat: create urls.py

---
+++ business_management/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+app_name = 'business_management'
+
+urlpatterns = [
+    path('/api/register', RegisterUser.as_view()),
+    path('/api/login', LoginView.as_view()),
+    path('/api/user-information/<slug:query_type>', UserInformation.as_view()),
+    path('/api/change-password', ChangePasswordView.as_view()),
+    path('/api/update-user', UpdateUserView.as_view()),
+]
