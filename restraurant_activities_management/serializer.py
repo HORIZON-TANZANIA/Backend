07e1a68940e82b9dd3d1c06bf88af0ebd89d95f4
Major2000 <majormadobe@gmail.com>
2023-11-25 15:06:38 +0300

    created serializers.py

---
+++ restraurant_activities_management/serializer.py
@@ -0,0 +1,136 @@
+from rest_framework import serializers
+from .models import *
+
+class RestraurantGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = "__all__"
+        depth = 2
+
+
+class RestraurantPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = [
+            "name",
+            "location",
+            'pic',
+            "reg_no",
+        ]
+        
+
+class CouponGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+        fields = "__all__"
+        depth = 2
+
+
+class CouponPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+        fields = [
+            "restraurant",
+            "start_amount",
+            "end_amount",
+            "coupon_no",
+            "point",
+        ]
+
+
+class AwardGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Award
+        fields = "__all__"
+        depth = 2
+
+
+class AwardPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Award
+        fields = [
+            "restraurant",
+            "product",
+            "points",
+            "pic"
+        ]
+
+
+class AwardCountGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AwardsCount
+        fields = "__all__"
+        depth = 2
+
+
+class AwardCountGetTopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AwardsCount
+        fields = "__all__"
+        depth = 3
+
+    @classmethod
+    def get_last_5_records(cls):
+        # Retrieve the last 5 records based on the 'created_at' field
+        return AwardsCount.objects.order_by('-created_at')[:5]
+
+
+class AwardCountPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AwardsCount
+        fields = [
+            "award",
+            "award_code",
+            "code_used_state",
+        ]
+
+        
+class CouponTransactionGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CouponTransaction
+        fields = "__all__"
+        depth = 2
+
+
+class CouponTransactionPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CouponTransaction
+        fields = [
+            "user",
+            "award",
+            "point_used",
+        ]
+        
+
+class UserRestraurantGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRestraurant
+        fields = "__all__"
+        depth = 2
+
+
+class UserRestraurantPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRestraurant
+        fields = [
+            "user",
+            "restraurant",
+            "total_points",
+        ]
+
+
+class TransactionGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transactions
+        fields = "__all__"
+        depth = 2
+
+
+class TransactionPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transactions
+        fields = [
+            "user",
+            "coupon",
+            "points_made",
+        ]
