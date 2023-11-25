1db0692d46705f4cbc1d3b876b9052e5c235b856
Major2000 <majormadobe@gmail.com>
2023-11-25 19:20:04 +0300

    update urls.py

--- business_management/urls.py
+++ business_management/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from .views import *
 
 app_name = 'business_management'
 
 urlpatterns = [
-    path('api/register', RegisterUser.as_view()),
-    path('api/login', LoginView.as_view()),
-    path('api/user-information/<slug:query_type>', UserInformation.as_view()),
-    path('api/change-password', ChangePasswordView.as_view()),
-    path('api/update-user', UpdateUserView.as_view()),
+     path('admin/', admin.site.urls),
+    path('auth/register', RegisterUser.as_view()),
+    path('auth/login', LoginView.as_view()),
+    path('auth/user-information/<slug:query_type>', UserInformation.as_view()),
+    path('auth/change-password', ChangePasswordView.as_view()),
+    path('auth/update-user', UpdateUserView.as_view()),
+    path('api/', include('restraurant_activities_management.urls'))
 ]
